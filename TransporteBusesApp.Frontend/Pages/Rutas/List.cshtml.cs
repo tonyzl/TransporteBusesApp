054b352301b488b2e41fa6208374a25ba7709742
seguraedwin5 <51142779+seguraedwin5@users.noreply.github.com>
2022-09-09 21:23:55 -0500

    Creacion List Rutas Page
    * Se crea el mockup de List Rutas

--- TransporteBusesApp.Frontend/Pages/Rutas/List.cshtml.cs
+++ TransporteBusesApp.Frontend/Pages/Rutas/List.cshtml.cs
@@ -4,11 +4,22 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
- 
+using TransporteBusesApp.Dominio;
+using TransporteBusesApp.Persistencia.AppRepositorios;
+
 namespace TransporteBusesApp.Frontend.Pages
 {
     public class ListRutasModel : PageModel
     {
+        private readonly IRepositorioRutas repositorioRutas;
+
+        [BindProperty]
+        public IEnumerable<Dominio.Rutas> Rutas { get; set; }
+
+        public ListRutasModel(IRepositorioRutas repositorioRutas)
+        {
+            this.repositorioRutas = repositorioRutas;
+        }
         public void OnGet()
         {
  
