664293aa1206a561e0e32d8c867b34b7fd2f3cbd
seguraedwin5 <51142779+seguraedwin5@users.noreply.github.com>
2022-09-09 21:22:30 -0500

    Inicio Implementacion Rutas
    *Se agregan Data Annotations a la Entidad Rutas
    * Se agregan Pages Create y Edit

--- TransporteBusesApp.Dominio/Entidades/Rutas.cs
+++ TransporteBusesApp.Dominio/Entidades/Rutas.cs
@@ -1,9 +1,22 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace TransporteBusesApp.Dominio{
     public class Rutas{
+        [Required,Key]
+        [Display(Name="Id")]
         public int id {get; set;}
-        public Estaciones origen {get; set;} 
-        public Estaciones destino {get; set;} 
+        [Required, ForeignKey("Estaciones")]
+        [Display(Name="Origen")]
+        public virtual Estaciones origen {get; set;} 
+
+        [Required, ForeignKey("Estaciones")]
+        [Display(Name="Destino")]
+        public virtual Estaciones destino {get; set;} 
+
+        [Required]
+        [Display(Name="Tiempo Estimado")]
         public int tiempo_estimado {get; set;} 
     }
 }
