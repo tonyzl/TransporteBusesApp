aa90e9d99155e385b15f50532bf648ea228b0df0
seguraedwin5 <51142779+seguraedwin5@users.noreply.github.com>
2022-09-09 19:28:40 -0500

    Implementacion interfaz
    se implementa la interfaz IRepositorioEstaciones

--- TransporteBusesApp.Frontend/Pages/Estaciones/Create.cshtml.cs
+++ TransporteBusesApp.Frontend/Pages/Estaciones/Create.cshtml.cs
@@ -4,11 +4,17 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
- 
+using TransporteBusesApp.Persistencia.AppRepositorios;
+
 namespace TransporteBusesApp.Frontend.Pages
 {
     public class FormEstacionesModel : PageModel
     {
+        private readonly IRepositorioEstaciones repositorioEstaciones;
+        public FormEstacionesModel(IRepositorioEstaciones repositorioEstaciones)
+        {
+            this.repositorioEstaciones = repositorioEstaciones;
+        }
         public void OnGet()
         {
  
