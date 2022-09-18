548feadf23115da01a35abbc406009d06e87bbb0
cazmarped <martinez77p@gmail.com>
2022-09-17 23:22:01 -0500

    Creación del formulario editar rutas

--- TransporteBusesApp.Frontend/Pages/Rutas/Edit.cshtml.cs
+++ TransporteBusesApp.Frontend/Pages/Rutas/Edit.cshtml.cs
@@ -6,24 +6,71 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
+using TransporteBusesApp.Dominio;
+using TransporteBusesApp.Persistencia.AppRepositorios;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
 namespace TransporteBusesApp.Frontend.Pages.Rutas
 {
     public class Edit : PageModel
     {
-       [TempData]
+        [BindProperty]
+        public Dominio.Rutas ruta { get; set; }
+
+        [BindProperty]
+        public List<SelectListItem> lst_origen { get; set; }
+        [BindProperty]
+        public List<SelectListItem> lst_destino { get; set; }
+        [TempData]
         public string mensaje_error { get; set; }
         [TempData]
         public string mensaje_guardado { get; set; }
-        private readonly ILogger<Edit> _logger;
-
-        public Edit(ILogger<Edit> logger)
+        public IRepositorioRutas _repositorioRutas { get; set; }
+        public IRepositorioEstaciones _repositorioEstaciones { get; set; }
+        public Edit(IRepositorioRutas repositorioRutas,IRepositorioEstaciones repositorioEstaciones)
         {
-            _logger = logger;
-            //Falta implementar Lógica
+            _repositorioRutas = repositorioRutas;
+            _repositorioEstaciones = repositorioEstaciones;
+            
         }
 
-        public void OnGet()
+        public void OnGet(int id)
         {
+            lst_origen = GetListEstaciones();
+            lst_destino = GetListEstaciones();
+
+               if(id > 0){
+                ruta = _repositorioRutas.GetWithId(id);
+
+            }
+            
+            
         }
+         public IActionResult OnPost(Dominio.Rutas ruta){
+            
+            if(ModelState.IsValid && ruta.origenid != ruta.destinoid){
+            _repositorioRutas.Update(ruta);
+            mensaje_guardado = "Ruta actualizada Correctamente";
+            return RedirectToPage("./List");
+            }else{
+                mensaje_error = "Error al actualizar la ruta valide que el origen y el destino no sean iguales";
+                //return Page(); //metodo para que el sistema mantenga el id  es decir la misma vista
+            return RedirectToPage("./List");
+            }
+         }
+        //metodo para llenar las listas de estaciones en la lista de rutas 
+        public List<SelectListItem> GetListEstaciones(){
+            var selectestaciones = new List<SelectListItem>();
+            List<Dominio.Estaciones> estaciones = _repositorioEstaciones.GetAll().ToList();
+            
+            selectestaciones = estaciones.Select(e => new SelectListItem
+            {
+                Value = e.id.ToString(),
+                Text = e.nombre
+            }).ToList();
+
+            return selectestaciones;
+        }
+
     }
 }
--- TransporteBusesApp.Persistencia/Migrations/20220918021031_Migracion2.Designer.cs
+++ TransporteBusesApp.Persistencia/Migrations/20220918021031_Migracion2.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TransporteBusesApp.Persistencia;
 
 namespace TransporteBusesApp.Persistencia.Migrations
 {
     [DbContext(typeof(AppdbContext))]
-    partial class AppdbContextModelSnapshot : ModelSnapshot
+    [Migration("20220918021031_Migracion2")]
+    partial class Migracion2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
