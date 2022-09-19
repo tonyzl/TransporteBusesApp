ceb1b9ab891ba0f363bdf8023bdf14030fba6d11
seguraedwin5 <51142779+seguraedwin5@users.noreply.github.com>
2022-09-19 03:21:38 -0500

    se implementa la funcion de busqueda desde el index

--- TransporteBusesApp.Frontend/Pages/Index.cshtml.cs
+++ TransporteBusesApp.Frontend/Pages/Index.cshtml.cs
@@ -5,21 +5,35 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using TransporteBusesApp.Persistencia.AppRepositorios;
+using TransporteBusesApp.Dominio;
 
 namespace TransporteBusesApp.Frontend.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        public IEnumerable<Dominio.Rutas> rutasfiltradas { get; set; }
+        private readonly IRepositorioRutas _reporutas;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(IRepositorioRutas reporutas)
         {
-            _logger = logger;
+            _reporutas = reporutas;
+        }
+        public void OnGet(List<Dominio.Rutas> rutasfiltradas)
+        {
+               if(rutasfiltradas != null)
+            {
+
+            } 
         }
 
-        public void OnGet()
+        public IActionResult OnPostFilterList(int id_origen, int id_destino)
         {
-
+            if (id_origen >0 && id_destino > 0) {
+                
+                return RedirectToPage("./Rutas/Estaciones/List",new{ idorigen = id_origen, iddestino = id_destino });
+            }
+            return Page();
         }
     }
 }
